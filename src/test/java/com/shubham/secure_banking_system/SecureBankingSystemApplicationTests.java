db6985083b307400d367915feefceb7f9a40d1c9
ShubhamDesaiHere <shubhamdesaihere@gmail.com>
2026-07-20 23:01:23 +0530

    spring boot setup change

---
+++ src/test/java/com/shubham/secure_banking_system/SecureBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.secure_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
