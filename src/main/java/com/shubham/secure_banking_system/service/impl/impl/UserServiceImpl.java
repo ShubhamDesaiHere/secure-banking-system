f5ad8e2d8cb509755f64fea9d8f8154b12bea6bb
ShubhamDesaiHere <shubhamdesaihere@gmail.com>
2026-07-31 22:59:57 +0530

    adding serives

---
+++ src/main/java/com/shubham/secure_banking_system/service/impl/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.shubham.secure_banking_system.service.impl;
+
+import com.shubham.secure_banking_system.dto.request.RegisterRequest;
+import com.shubham.secure_banking_system.dto.response.RegisterResponse;
+import com.shubham.secure_banking_system.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public RegisterResponse register(RegisterRequest request) {
+
+        return null;
+    }
+}
